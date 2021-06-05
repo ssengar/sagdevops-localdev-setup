a5f65e27bf42d219b5902ca12d920034663b2631
Sengar <seng@softwareag.com>
2021-06-05 20:00:29 +0530

    added copyfile.bat

--- scripts/copy_file.bat
+++ scripts/copy_file.bat
@@ -8,22 +8,39 @@ rem #
 rem #############################################
 
 rem Read CCE and CICD setup related values from a properties file 
-FOR /F "eol=# tokens=1,2 delims==" %%G IN (../config/ldev-setup.properties) DO (set %%G=%%H)  
+FOR /F "eol=# tokens=1,2 delims==" %%G IN (..\config\ldev-setup.properties) DO (set %%G=%%H)  
 
 SET src_dir=%src_dir%
 SET target_dir=%target_dir%
+echo local dev directory:%src_dir%
+echo sagdevops CCE directory:%target_dir%
 IF NOT EXIST %src_dir% MKDIR %src_dir%
-git clone https://github.com/saigandi/sagdevops-localdev-setup.git
+cd %src_dir%
+git -C %src_dir%/sagdevops-localdev-setup pull || git clone https://github.com/saigandi/sagdevops-localdev-setup.git
 echo Clone the sagdevops-cc-server GIT repo
 echo.
 IF NOT EXIST %target_dir% MKDIR %target_dir%
-git clone --recursive -b release/105oct2019 https://github.com/SoftwareAG/sagdevops-cc-server 
+cd %target_dir%
+git -C %target_dir%/sagdevops-cc-server pull || git clone --recursive -b release/105oct2019 https://github.com/SoftwareAG/sagdevops-cc-server 
 echo Clone the ldev-setup GIT repo
 echo.
 xcopy /s /e "%src_dir%/sagdevops-localdev-setup/templates" "%target_dir%/sagdevops-cc-server/templates"
+IF %errorlevel% NEQ 0 (
+	echo Non-zero exit code %errorlevel% was returned. Exit the process.
+	rem endlocal	
+	EXIT /B %errorlevel%
+)
+echo custom templates copied successfully
 cd %target_dir%/sagdevops-cc-server/antcc
-mkdir environments/default
+mkdir environments
+cd %target_dir%/sagdevops-cc-server/antcc/environments
+mkdir default
 copy /-y "%src_dir%/sagdevops-localdev-setup/environments/default" "%target_dir%/sagdevops-cc-server/antcc/environments/default"
-copy /-y "%src_dir%/sagdevops-localdev-setup/config/sagenv.xml" "%target_dir%/sagdevops-cc-server/antcc/lib"
-EN
+copy /y "%src_dir%/sagdevops-localdev-setup/config\sagenv.xml" "%target_dir%/sagdevops-cc-server/antcc/lib"
+IF %errorlevel% NEQ 0 (
+	echo Non-zero exit code %errorlevel% was returned. Exit the process.
+	rem endlocal	
+	EXIT /B %errorlevel%
+)
+ENDLOCAL
 EXIT /B %errorlevel%
